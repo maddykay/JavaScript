fc9bd64fbfafad302c34e3ede554a5ec3d30df42
mthomas391 <51038116+mthomas391@users.noreply.github.com>
2019-10-26 13:18:37 -0700

    JavaScript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // Create a variable below
-
+var helloWorld = "Hello World";
 // Console.log() your variable here.
+console.log(helloWorld);
+// Create the addNumbers function here.
+function addNumbers(){
+    var two=2;
+    var three=3;
+    var five= two+three;
+    console.log (five)
+}
+
+// Create the stringConcat function here.
+addNumbers();
+function stringContact(){
+    var taco="taco";
+    var cat="cat";
+    var tacoCat='taco' + '' + 'cat';
+}
+
+alert('tacoCat')
